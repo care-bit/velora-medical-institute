12fd251b6893e16f81811404d47d6a1ae15433d3
care-bit <care@veloramedicine.com>
2026-06-08 13:34:27 -0500

    Update booking-client.tsx

--- app/book/booking-client.tsx
+++ app/book/booking-client.tsx
@@ -366,7 +366,7 @@ window.location.href =
                       Back
                     </button>
                     <a
-  href="PASTE_YOUR_CHARMHEALTH_BOOKING_LINK_HERE"
+  href="https://ehr.charmtracker.com/publicCal.sas?method=getCal&digest=169f4dd01960b6c347cce5b694b4ef068939d9c126dfbd091753f82206e0aa9e4b5cb98c3566431f7b16a2ad35a972fba0ae868b6eb0918a"
   className="btn-primary"
 >
   Continue to Secure Scheduling
