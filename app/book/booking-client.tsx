cb00c932fef67c5c1c2eb9e8e51e4f0a12b37de3
care-bit <care@veloramedicine.com>
2026-06-09 00:03:53 -0500

    Update booking-client.tsx

--- app/book/booking-client.tsx
+++ app/book/booking-client.tsx
@@ -113,7 +113,7 @@ export function BookingClient() {
     
       trackEvent('book_consultation', { visit_type: type })
 
-window.location.href = "https://ehr.charmtracker.com/publicCal.sas?method=getCal&digest=169f4dd01960b6c31cb1f577544c70e574ed96d3d580cebc1192a3b4b068b2f4c791341ca9a05ba47b16a2ad35a972fba0ae868b6eb0918a";
+window.location.href = "https://ehr.charmtracker.com/publicCal.sas?method=getCal&digest=169f4dd01960b6c31cb1f577544c70e574ed96d3d580cebc1192a3b4b068b2f4c791341ca9a05ba47b16a2ad35a972fba0ae868b6eb0918a"; 
     } catch {
       toast.error('Something went wrong', {
         description: 'Please try again or contact care@veloramedicalinstitute.com',
